bab994ede95e168d3b452a989e121a3b24fd8678
zayedu <umerzayed1@gmail.com>
2024-01-18 23:50:50 -0500

    Completed Walking Skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,38 +1,31 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.commons.cli.Options;
 
-public class Maze{
-  public final File PATH_FILE;
-  public Maze(File fileName){
-    this.PATH_FILE = fileName;
-  }
-  public void printMaze() throws IOException{
-    BufferedReader reader = new BufferedReader(new FileReader(this.PATH_FILE));
-    String line;
-    while ((line = reader.readLine()) != null) {
-      for (int idx = 0; idx < line.length(); idx++) {
-        if (line.charAt(idx) == '#') {
-          System.out.print("WALL ");
-        } else if (line.charAt(idx) == ' ') {
-          System.out.print("PASS ");
-        }
-      }
-      System.out.print(System.lineSeparator());
+public class Maze {
+    private final String path;
+    public Maze(String path) {
+        this.path= path;
     }
-  }
-  private MazeRunner get_runner(){
-    return new MazeRunner(this.PATH_FILE);
-  }
-  public MazePath getPath(){
-    return this.get_runner().getPath();
-  }
-  public boolean verifyPath(MazePath path){
-    return this.get_runner().navigate(path);
-  }
+
+    public void printMaze() throws IOException {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (int idx = 0; idx < line.length(); idx++) {
+                    if (line.charAt(idx) == '#') {
+                        System.out.print("WALL ");
+                    } else if (line.charAt(idx) == ' ') {
+                        System.out.print("PASS ");
+                    }
+                }
+                System.out.print(System.lineSeparator());
+            }
+
+    }
 }
